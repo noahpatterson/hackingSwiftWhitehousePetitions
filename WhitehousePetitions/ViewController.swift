c5300afbba7e0467ec2a4a47cc892dfe937be9da
noahpatterson <n@noahpatterson.me>
2016-12-10 15:43:24 -0700

    + create petitions array + set cell textLabel and detailTextLabel with placeholders

--- WhitehousePetitions/ViewController.swift
+++ WhitehousePetitions/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,17 @@ class ViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text       = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        
+        return cell
+    }
 }
 
